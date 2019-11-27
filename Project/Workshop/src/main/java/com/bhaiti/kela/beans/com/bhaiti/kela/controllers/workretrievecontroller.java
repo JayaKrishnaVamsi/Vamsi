8450f182fbac5d0e09852091dd9077831344b4b3
JayaKrishnaVamsi <55281736+JayaKrishnaVamsi@users.noreply.github.com>
2019-11-27 16:31:46 +0530

    Add files via upload

--- Project/Workshop/src/main/java/com/bhaiti/kela/beans/com/bhaiti/kela/controllers/workretrievecontroller.java
+++ Project/Workshop/src/main/java/com/bhaiti/kela/beans/com/bhaiti/kela/controllers/workretrievecontroller.java
@@ -1,4 +1,6 @@
 package com.bhaiti.kela.controllers;
+
+
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +14,22 @@ public class workretrievecontroller {
   @ResponseBody
   public List<work> getAllWorks() {
   return registration.getInstance().getworkRecords();
+ /* int i=0;
+  List<work> getworks=registration.getInstance().getworkRecords();
+ try { Class.forName("com.mysql.jdbc.Driver");
+  Connection con = DriverManager.getConnection("jdbc:mysql//localhost:3306/workshop","root","vamsi4mysql");
+  Statement stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+  ResultSet rs=stmt.executeQuery("Select * from workshop");
+  while(rs.next())
+  {
+	  getworks.get(i).setWorkid(rs.getString(1));
+	  getworks.get(i).setTopic(rs.getString(2));
+	  getworks.get(i).setVenue(rs.getString(3));
+	  i++;
   }
-}
+  }
+ catch(Exception e) {}
+  return getworks;
+  }
+*/
+}}
---
+++ Project/Workshop/target/classes/workshop.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE workshop (
+  
+	work_id varchar(45) PRIMARY KEY, 
+	topic varchar(45), 
+	venue varchar(45), 
+	Date date, 
+	organiser varchar(45), 
+	reg_fee varchar(45), 
+	location varchar(45)
+);
